5590131e243ecef78779cce3dfb4f2840b853d54
Glitch (hall500-boilerplate-project-headerparser) <none>
2020-08-18 04:05:47 +0000

    🐓👗 Checkpoint ./server.js:10219482/167

--- server.js
+++ server.js
@@ -24,7 +24,11 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
-
+app.get("/api/whoami", function(req, res){
+  res.json({
+    "request": req
+  });
+});
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
